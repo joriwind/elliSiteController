7750743c6663a6cef9b86eac35a0554e99e74095
joriwind <jori.winderickx@student.uhasselt.be>
2015-03-27 14:12:35 +0000

    Added

---
+++ dtlsCoapServer/dtls.js
@@ -0,0 +1,17 @@
+var ffi = require('ffi');
+
+var dtls = function(){
+   
+}
+
+
+
+var dtls-interface = ffi.Library('dtls-interface', {
+  'init': [ 'double', [ 'double' ] ],
+  '
+});
+
+
+dtls.prototype.createServer(options, callback){
+   
+}
