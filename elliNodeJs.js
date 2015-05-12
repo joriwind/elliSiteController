1cfd356e638d25c9611edb4eb8c2806d7e3b4a64
joriwind <jori.winderickx@student.uhasselt.be>
2015-05-12 07:43:55 +0000

    The node in js

---
+++ elliNodeJs.js
@@ -0,0 +1,66 @@
+var coap        = require('coap');
+var Server  = require('./dtlsCoap/dtlsServer');
+var server      = new Server({ type: 'udp6' , eccCert: './certs/client-ecc-cert.pem',
+                                 ourCert: './certs/server-ecc.pem', ourKey:'./certs/ecc-key.pem'})
+
+server.on('request', function(req, res) {
+   var route = req.url.split('/')[1];
+   console.log("Received message from client: " + route  + '\nMessage payload:S:' + req.payload+':E\n');
+  
+   //Parse packet
+   try{
+   var js = JSON.parse(req.payload);
+   console.log("Packet parsed: " + JSON.stringify(js));
+   }catch (e){
+     //Could not parse JSON -> send error to client
+     console.log(e);
+     res.code = '4.06';
+     return res.end();
+   }
+   
+   //Endpoints
+   switch(route){
+      case 'ping':
+         console.log("Ping route");
+         return res.end('Hello client\n');
+         break;
+      default:
+         console.log("Default, route: " + route);
+         res.code = '4.01';
+         break;
+     
+   }
+  
+   res.end();
+  
+})
+
+server.on('error', function(err){
+   console.log("Error in server: "+ err);
+});
+
+// the default CoAP port is 5683
+server.listen(11111, function() {
+   
+});
+
+server.on('awaitingConnection', function(){
+   console.log("Awainting an connection");
+   //Initialize Agent, setup ctx --> ready to establish connection.
+   
+   var req = coap.request('coap://[::1]/');
+   var payload = {name: 'node1', type: 'ell-i'}
+   
+   req.write(JSON.stringify(payload));
+   
+   req.on('error', function(err){
+      console.log("Somehting went wrong in service announcement: " + err);
+   });
+   
+   req.on('response', function(res){
+      console.log("Response of something: " + JSON.stringify(res));
+      
+   });
+   
+   req.end();
+});
